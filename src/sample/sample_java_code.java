5bfb98def85d128c937fd763f6ac74f1cf242385
ravisankar.n <ravisankar.n@AZR-HQ-yPEmnq6w.HQ.GlobalCashAccess.us>
2024-07-17 20:05:43 +0530

    Initial commit

---
+++ src/sample/sample_java_code.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class sample_java_code {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 4;
+		int j = 7;
+		System.out.println(i+j);
+		System.out.println("Testing");
+	}
+
+}
